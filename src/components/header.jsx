ca96b233c33844cb1fd5abebfcbf70bfda1e514e
MariaNamata086 <namatamaria086@gmail.com>
2023-04-18 05:46:31 +0300

    first commit for this project. Work on header section.

---
+++ src/components/header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from 'next/link'
+import { MenuItem } from './menu-item'
+import { AiFillHome } from "react-icons/ai";
+import { BsInfoCircleFill } from "react-icons/bs";
+const Header = () => {
+  return (
+    <div className='flex flex-row w-[85%] mx-auto items-center justify-between'>
+        <div className='flex mt-6 mb-6 gap-12'>
+            <MenuItem title='HOME' address= '/' Icon={AiFillHome}/>
+            <MenuItem title='ABOUT' address= '/about' Icon={BsInfoCircleFill}/>
+        </div>
+        <div className='flex'>
+         <div></div>
+         <Link href='/'>
+           <div className='flex items-center tracking-wider'>
+             <span className='p-2 py-1 px-3 rounded-lg bg-amber-500 font-bold text-2xl'>IMDb</span>
+             <span className='text-lg ml-1 hidden sm:block'>clone</span>
+           </div>
+         </Link> 
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/menu-item.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+export const MenuItem = ({title,address,Icon}) => {
+  return (
+    <div>
+        <Link href={address} className='hover:text-amber-600' >
+            <Icon className = 'text-2xl md:hidden '/>
+            <span className='hidden md:block my-2 text-sm'> 
+            {title}
+            </span>
+            </Link>
+    </div>
+  )
+}
