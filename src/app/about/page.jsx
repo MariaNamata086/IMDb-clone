be0bd032b835e45ae8653cd97c729c15472268d0
MariaNamata086 <namatamaria086@gmail.com>
2023-04-26 00:14:09 +0300

    fix deployment error by escaping double quotes with &#34;

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -11,7 +11,7 @@ const About = () => {
         The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text did not listen. She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
         <p className='text-lg tracking-wide leading-wide'>
          When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.
-          On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country.
+          On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word &#34;and&#34; and the Little Blind Text should turn around and return to its own, safe country.
         </p>
        
         </div>
